940c71b2bcad98f3c28c8ba04b3b07bc108c70d4
FaizalRoySusilo <165460795+FaizalRoySusilo@users.noreply.github.com>
2024-05-02 21:22:57 +0700

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/AppCount.dart';
+import 'package:coba/login.dart';
+
+
 
 void main() {
   runApp(const MainApp());
@@ -11,7 +13,7 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: AppCounter()
+      home: LoginApp()
     );
   }
 }
